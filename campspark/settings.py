b140a4bdbb1a5e7c1e7d9bb28f88843ca45639b2
Dominic Savio <7dsavio@gmail.com>
2026-03-05 21:26:41 +0530

    enable debug for error checking

--- campspark/settings.py
+++ campspark/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
